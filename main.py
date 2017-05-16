aed7a92cb1944561ce2a15c155ec97b6d80952ad
Kevin Robert Stravers <macocio@gmail.com>
2017-05-16 21:07:21 +0200

    Avoid downloading items that have an empty filename= tag
    This would crash my download with an IndexError because, well, the list was empty.

--- main.py
+++ main.py
@@ -167,7 +167,11 @@ def download_file(directory, link_url, session):
                    fromstring(file_page.content).xpath(
                        '//a[@class="ccl-button ccl-button-color-green ccl-button-submit"]/@href')[0][2:]
     download = session.get(download_url, stream=True)
-    raw_file_name = re.findall('filename="(.+)"', download.headers['content-disposition'])[0]
+    raw_file_name = re.findall('filename="(.+)"', download.headers['content-disposition'])
+    if raw_file_name:
+        raw_file_name = raw_file_name[0]
+    else:
+        return
     filename = raw_file_name.encode('iso-8859-1').decode()
     filepath = os.path.join(directory, filename)
     with open(filepath, 'wb') as downloaded_file:
